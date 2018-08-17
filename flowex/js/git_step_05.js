324962813b81260697a6ffa79e286a0e416425c7
Julieta <hernan.morales@gmail.com>
2018-08-17 16:16:38 -0300

    Continue workflow...

---
+++ flowex/js/git_step_05.js
@@ -0,0 +1,82 @@
+var graphConfig = new GitGraph.Template({
+  colors: [ "#9993FF", "#47E8D4", "#6BDB52", "#F85BB5", "#FFA657", "#F85BB5" ],
+  branch: {
+    color: "#000000",
+    lineWidth: 3,
+    spacingX: 60,
+    mergeStyle: "straight",
+    showLabel: true, // display branch names on graph
+    labelFont: "normal 12pt Arial",
+    labelRotation: 0
+  },
+  commit: {
+    spacingY: -100,
+    dot: {
+      size: 25,
+      strokeColor: "#000000",
+      strokeWidth: 4
+    },
+    tag: {
+      font: "normal 12pt Arial",
+      color: "yellow"
+    },
+    message: {
+      color: "black",
+      font: "normal 12pt Arial",
+      displayAuthor: false,
+      displayBranch: false,
+      displayHash: false,
+    }
+  },
+  arrow: {
+    size: 25,
+    offset: 1
+  }
+});
+
+var config = {
+  template: graphConfig,
+  mode: "extended",
+  orientation: "horizontal"
+};
+
+// You can manually fix columns to control the display.
+var featureCol = 0;
+var developCol = 1;
+var releaseCol = 2;
+var supportCol = 3;
+var support2Col = 5;
+var masterCol = 4;
+
+var gitgraph = new GitGraph(config);
+
+var master = gitgraph.branch({
+  name: "master",
+  column: masterCol
+});
+master.commit("Initial commit");
+
+master.commit({
+  messageDisplay: false
+});
+
+var develop = gitgraph.branch({
+  parentBranch: master,
+  name: "desarrollo",
+  column: developCol
+});
+
+develop.commit({
+  messageDisplay: false
+});
+
+var feature1 = gitgraph.branch({
+  parentBranch: develop,
+  name: "feature/1",
+  column: featureCol
+});
+feature1.commit("A feature to go into v1.0.0").commit({
+  messageDisplay: false
+});
+
+feature1.merge(develop);
